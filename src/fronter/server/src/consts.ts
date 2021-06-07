2d07aa90d15071a3124c6c346f1d02f7dd8ed6b5
Linus Ljung <linus.ljung@outlook.com>
2021-06-07 20:44:30 +0200

    Create new user in DB if needed. Use subject field in JWT as identifier

--- src/fronter/server/src/consts.ts
+++ src/fronter/server/src/consts.ts
@@ -5,3 +5,6 @@ export const SESSION_HOST = process.env.SESSION_HOST;
 export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 export const SESSION_SECRET = process.env.SESSION_SECRET;
+
+export const DB_HOST = process.env.DB_HOST;
+export const DB_NAME = process.env.DB_NAME;
---
+++ src/fronter/server/src/database/user.ts
@@ -0,0 +1,47 @@
+import { Collection, COLLECTION_NAMES, Db, FilterQuery, useDb, User } from '@linusljung/use-db';
+import { DB_HOST, DB_NAME } from '../consts';
+
+function getCollection(db: Db) {
+  return db.collection<User>(COLLECTION_NAMES.users);
+}
+
+function create(collection: Collection<User>, id: string, email: string) {
+  const document: Omit<User, '_id'> = {
+    id,
+    email,
+    subscriptions: [],
+  };
+
+  return collection.insertOne(document).then((user) => user.ops[0]);
+}
+
+export function getOrCreate(id: string, email: string) {
+  return new Promise<User>((resolve, reject) => {
+    useDb(DB_HOST!, DB_NAME!, (error, db) => {
+      return new Promise((useDbResolve, useDbReject) => {
+        if (error) {
+          reject(error);
+          useDbReject(error);
+
+          return;
+        }
+
+        db = db!;
+
+        const filter: FilterQuery<User> = {
+          id,
+        };
+
+        const collection = getCollection(db);
+
+        collection.findOne<User>(filter).then((user) => {
+          if (!user) {
+            return resolve(create(collection, id, email));
+          }
+
+          resolve(user);
+        });
+      });
+    });
+  });
+}
