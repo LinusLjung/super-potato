f0d8f52258d6dd649ed50ca5b7fb50ac9f650972
Linus Ljung <linus.ljung@outlook.com>
2021-06-08 20:00:42 +0200

    Moved the .env file to the project root

--- src/fronter/server/src/consts.ts
+++ src/fronter/server/src/consts.ts
@@ -1,10 +1,11 @@
-export const PORT = process.env.PORT;
-export const HOST = process.env.HOST;
-export const SESSION_HOST = process.env.SESSION_HOST;
+export const PORT = process.env.PORT || 3001;
+export const HOST = process.env.HOST || 'localhost';
 
 export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+
+export const SESSION_HOST = process.env.SESSION_HOST || 'localhost';
 export const SESSION_SECRET = process.env.SESSION_SECRET;
 
-export const DB_HOST = process.env.DB_HOST;
-export const DB_NAME = process.env.DB_NAME;
+export const DB_HOST = process.env.DB_HOST || 'localhost';
+export const DB_NAME = process.env.DB_NAME || 'super-potato';
