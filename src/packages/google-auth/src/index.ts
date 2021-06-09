40821eabbd07826579a023a2d0f7ca15451b89bf
Linus Ljung <linus.ljung@outlook.com>
2021-06-09 21:35:27 +0200

    Export AuthDataType from 'use-db'

--- src/packages/google-auth/src/index.ts
+++ src/packages/google-auth/src/index.ts
@@ -1,4 +1,4 @@
 export { default as getAuthEndpointUrl } from './get-auth-endpoint-url';
 export { default as getOpenIDConfiguration } from './get-openid-configuration';
 export { getTokenEndpointUrl, getTokenEndpointBody } from './token-endpoint';
-export { default as validateJWT } from './validate-jwt';
+export { default as validateJWT, AuthDataType } from './validate-jwt';
